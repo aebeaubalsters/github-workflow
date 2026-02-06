d48b594b15e6bd4bce9174a6d5fa826563e77dee
aebeaubalsters <beau.balsters@advisorsexcel.com>
2026-02-06 15:46:25 -0600

    Comment out svc-beau-test

--- IAM/accounts/service/main.tf
+++ IAM/accounts/service/main.tf
@@ -1,6 +1,6 @@
 locals {
   service_users = {
-    svc-beau-test = { name = "svc-beau-test", policy_arns = {}, policy_description = "Beau's test service user for restarting ec2 instances" },
+    //svc-beau-test = { name = "svc-beau-test", policy_arns = {}, policy_description = "Beau's test service user for restarting ec2 instances" },
     //key = { name = "service-user-name", policy_arns = { arbitrary_key = Policy ARNs of any existing policies you want to add to this User (such as default AWS created policies) }, policy_description = "Policy description for the generated policy in IAM/accounts/service/account-name" }
   }
 }
