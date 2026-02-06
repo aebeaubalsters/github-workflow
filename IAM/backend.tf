8f434843e50dadaf2e861d30472600a84f37edd4
aebeaubalsters <beau.balsters@advisorsexcel.com>
2026-02-06 13:59:24 -0600

    update backend key

--- IAM/backend.tf
+++ IAM/backend.tf
@@ -1,7 +1,7 @@
 terraform {
      backend "s3" {
     bucket         = "sandbox-iac-remote-state"
-    key            = "#{aws.beautest.region}/beautest/backend.tfstate"
+    key            = "#{aws.beautest.region}/iam/backend.tfstate"
     region         = "#{aws.beautest.region}"
     dynamodb_table = "sandbox-iac-state-locking"
     profile        = "#{aws.beautest.profile}"
